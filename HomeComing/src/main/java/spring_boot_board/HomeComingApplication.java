75caa122376e4e9ae43d0da3ed9a2325dce1ca4b
sung0506 <[sung990506@naver.com]>
2024-11-21 11:37:44 +0900

    홈 화면 추가

--- HomeComing/src/main/java/spring_boot_board/HomeComingApplication.java
+++ HomeComing/src/main/java/spring_boot_board/HomeComingApplication.java
@@ -2,12 +2,19 @@ package spring_boot_board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 @SpringBootApplication
 public class HomeComingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomeComingApplication.class, args);
+	}
+	@GetMapping("/")
+	public String index() {
+		return "thymeleaf/index";
 	}
 
 }
