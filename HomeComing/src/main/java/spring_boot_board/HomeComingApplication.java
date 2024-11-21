1baccf02e92b7e4676fc51174c11b57903c87b68
sung0506 <[sung990506@naver.com]>
2024-11-21 11:19:34 +0900

    HomeComing

---
+++ HomeComing/src/main/java/spring_boot_board/HomeComingApplication.java
@@ -0,0 +1,13 @@
+package spring_boot_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeComingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeComingApplication.class, args);
+	}
+
+}
---
+++ HomeComing/src/test/java/spring_boot_board/HomeComingApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_boot_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeComingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
